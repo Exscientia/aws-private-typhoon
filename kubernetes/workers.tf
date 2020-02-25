3d4674af59b655bbf5259dc69aa99fa2ca957032
Liam Humphreys <liam.humphreys1989@gmail.com>
2020-02-25 10:37:00 +0000

    add cluster_name to workers inputs rm os_image add iam worker node role & policy to allow dynamic cluster

--- kubernetes/workers.tf
+++ kubernetes/workers.tf
@@ -1,17 +1,18 @@
 module "workers" {
-  source = "./workers"
-  name   = var.cluster_name
+  source       = "./workers"
+  name         = var.cluster_name
+  cluster_name = var.cluster_name
 
   # AWS
-  vpc_id          = aws_vpc.network.id
-  subnet_ids      = aws_subnet.public.*.id
-  security_groups = [aws_security_group.worker.id]
-  worker_count    = var.worker_count
-  instance_type   = var.worker_type
-  os_image        = var.os_image
-  disk_size       = var.disk_size
-  spot_price      = var.worker_price
-  target_groups   = var.worker_target_groups
+  vpc_id                          = aws_vpc.network.id
+  subnet_ids                      = aws_subnet.public.*.id
+  security_groups                 = [aws_security_group.worker.id]
+  worker_count                    = var.worker_count
+  instance_type                   = var.worker_type
+  disk_size                       = var.disk_size
+  spot_price                      = var.worker_price
+  target_groups                   = var.worker_target_groups
+  worker_iam_instance_profile_arn = aws_iam_instance_profile.worker_node.arn
 
   # configuration
   kubeconfig            = module.bootstrap.kubeconfig-kubelet
@@ -20,5 +21,73 @@ module "workers" {
   cluster_domain_suffix = var.cluster_domain_suffix
   clc_snippets          = var.worker_clc_snippets
   node_labels           = var.worker_node_labels
+
+  tags = var.tags
 }
 
+
+resource "aws_iam_instance_profile" "worker_node" {
+  name = "${var.cluster_name}-worker-node"
+  role = aws_iam_role.worker_node.name
+}
+
+resource "aws_iam_role" "worker_node" {
+  name                  = "${var.cluster_name}-worker-node"
+  force_detach_policies = true
+  assume_role_policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = merge(var.tags, {
+    "kubernetes.io/cluster/${var.cluster_name}" : "shared"
+  })
+}
+
+resource "aws_iam_policy" "worker_node" {
+  name = "${var.cluster_name}-worker-node"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeInstances",
+      "ec2:DescribeRegions",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:BatchGetImage",
+      "elasticfilesystem:*"
+    ],
+    "Resource": "*"
+  }]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+  role       = aws_iam_role.worker_node.name
+  policy_arn = aws_iam_policy.worker_node.arn
+}
