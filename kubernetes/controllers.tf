c9b3cbaac52fb5f7f8131bcd5fbbc9d9c79b5437
Liam Humphreys <liam.humphreys1989@gmail.com>
2020-02-27 14:13:24 +0000

    put controllers in public subnets in round robin fashion

--- kubernetes/controllers.tf
+++ kubernetes/controllers.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "controllers" {
 
   # network
   associate_public_ip_address = false
-  subnet_id                   = module.vpc.private_subnets[0]
+  subnet_id                   = element(module.vpc.public_subnets, count.index)
   vpc_security_group_ids      = [aws_security_group.controller.id]
 
   iam_instance_profile = aws_iam_instance_profile.controller_node.name
