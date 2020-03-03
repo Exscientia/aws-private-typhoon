9156445cf3b1f4b8b15fff49fafaf1c0bb94448e
Liam Humphreys <liam.humphreys1989@gmail.com>
2020-03-03 16:35:39 +0000

    probably quite evident that I don't know what I'm doing by now

--- kubernetes/ssh.tf
+++ kubernetes/ssh.tf
@@ -16,11 +16,11 @@ resource "null_resource" "copy-controller-secrets" {
   ]
 
   connection {
-    type    = "ssh"
-    host    = aws_instance.controllers.*.private_ip[count.index]
-    user    = "core"
-    timeout = "15m"
-    # private_key = tls_private_key.bastion.private_key_pem
+    type        = "ssh"
+    host        = aws_instance.controllers.*.private_ip[count.index]
+    user        = "core"
+    timeout     = "15m"
+    private_key = tls_private_key.bastion.private_key_pem
 
     bastion_host        = aws_instance.bastion.public_ip
     bastion_user        = "alpine"
